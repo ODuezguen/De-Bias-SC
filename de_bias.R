80c432b742adfdbb9602770f956ee00fa95d91aa
Onur Düzgün <77804721+oduzgun@users.noreply.github.com>
2022-12-19 23:39:04 +0100

    Update de_bias.R

--- de_bias.R
+++ de_bias.R
@@ -1,26 +1,43 @@
 #' This function calculates the bias term given the inputs:
 #' 
-#' X0: This is a matrix of feature values for the control group.
-#' X1: This is a matrix of feature values for the treatment group.
-#' Y: This is a vector of values that we want to predict using the feature matrices X0 and X1.
-#' D: This is a binary vector indicating membership in one of the two groups represented by X0 and X1.
-#' weights: This is a n1 x n0 matrix of weights that will be used to adjust the predicted values.
+#' X0: This is a (k x n0) matrix of feature values for the control group.
+#' X1: This is a (k x n1) matrix of feature values for the treatment group.
+#' Y: This is a (T x n1+n0) matrix of values that we want to predict using the feature matrices X0 and X1.
+#' D: This is a binary vector of length (n1+n0) indicating membership in one of the two groups.
+#' W: This is a (n1 x n0) matrix of weights that will be used to adjust the predicted values.
 
-de_bias <- function(X0, X1, Y, D, weights){
-  # Calculate the number of columns in each matrix using colSums
-  num_cols0 <- colSums(X0 != 0)
-  num_cols1 <- colSums(X1 != 0)
+de_bias <- function(X0, X1, Y, D, W){
+  
+  # Check that input matrices and vectors have the correct dimensions
+  if (!is.matrix(X0) || !is.matrix(X1) || !is.matrix(Y) || !is.vector(D)) {
+    stop("Inputs must be matrices and D must be a vector")
+  }
+  if (nrow(X0) != nrow(X1)) {
+    stop("X0 and X1 must have the same number of rows")
+  }
+  if (length(D) != ncol(X0) + ncol(X1)) {
+    stop("D must have the same length as the total number of columns in X0 and X1")
+  }
   
   # Create augmented feature matrices by setting the last element of each column to 1
-  augmented_X0 <- cbind(X0, 1)
-  augmented_X1 <- cbind(X1, 1)
+  augmented_X0 <- cbind(X0, rep(1, ncol(X0)))
+  augmented_X1 <- cbind(X1, rep(1, ncol(X1)))
   
   # Calculate the weight matrix using the inverse of the transpose of augmented_X0 multiplied by augmented_X_0
-  weight_matrix <- solve(t(augmented_X0) %*% augmented_X0) %*% augmented_X0
+  weight_matrix <- tryCatch(
+    solve(t(augmented_X0) %*% augmented_X0) %*% augmented_X0,
+    error = function(e) {
+      warning("Error calculating weight matrix. Using default value.")
+      return(matrix(1, nrow(X0), ncol(X0)))
+    }
+  )
   
   # Vectorize the calculation of bias
+  dbias = matrix(nrow=nrow(Y),ncol=ncol(X1))
+  for(t in 1:nrow(Y)){
   mean0 <- weight_matrix %*% Y[D==0]
-  de_bias <- t(augmented_X1) %*% mean0 - weights %*% (t(augmented_X0) %*% mean0)
+  dbias[t,] <- t(augmented_X1) %*% mean0 - W %*% (t(augmented_X0) %*% mean0)
+  }
   
-  return(de_bias)
+  return(dbias)
 }
