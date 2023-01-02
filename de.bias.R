b610b4f515b05af7f34ad4ea1a38e0612adf23a4
Onur Düzgün <onur.duezguen@uni-hamburg.de>
2023-01-02 13:00:55 +0100

    Update de.bias.R

--- de.bias.R
+++ de.bias.R
@@ -2,7 +2,7 @@
 #' 
 #' @params X0: This is a (k x n0) matrix of feature values for the control group.
 #' @params X1: This is a (k x n1) matrix of feature values for the treatment group.
-#' @params Y: This is a (T x n1+n0) matrix of values that we want to predict using the feature matrices X0 and X1.
+#' @params Ypre: This is a (T0 x n1+n0) matrix of pre-treatment outcome values.
 #' @params D: This is a binary vector of length (n1+n0) indicating membership in one of the two groups.
 #' @params W: This is a (n1 x n0) matrix of weights that will be used to adjust the predicted values.
 #'
@@ -10,10 +10,10 @@
 #'
 #' @author Onur Düzgün
 
-de.bias <- function(X0, X1, Y, D, W){
+de.bias <- function(X0, X1, Ypre, D, W){
   
   # Adding some checks
-  if (!is.matrix(X0) || !is.matrix(X1) || !is.matrix(Y) || !is.vector(D)) {
+  if (!is.matrix(X0) || !is.matrix(X1) || !is.matrix(Ypre) || !is.vector(D)) {
     stop("Inputs must be matrices and D must be a vector")
   }
   if (nrow(X0) != nrow(X1)) {
@@ -36,9 +36,10 @@ de.bias <- function(X0, X1, Y, D, W){
     }
   )
   
-  # Pre-allocating dbias and calculating it
-  dbias = matrix(nrow=nrow(Y),ncol=ncol(X1))
-  mean0 <- weight_matrix %*% Y[,D==0]
+  # Pre-allocating dbias
+  dbias = matrix(nrow=nrow(Ypre),ncol=ncol(X1))
+  # Calculating dbias given the weights
+  mean0 <- weight_matrix %*% Ypre[,D==0]
   dbias <- t(augmented_X1) %*% mean0 - W %*% (t(augmented_X0) %*% mean0)
   
   return(dbias)
